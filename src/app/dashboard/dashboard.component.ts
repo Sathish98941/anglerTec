57c3518ae8863b4c93b22c717d7cecfd7d54d32a
Sathish98941 <satishkumar.gopal@adcltech.com>
2021-10-11 14:19:09 +0530

    search

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,8 +4,6 @@ import { MatTableDataSource } from '@angular/material/table';
 import { DashboardService } from '../service/dashboard.service';
 import {SelectionModel} from '@angular/cdk/collections';
 import { noop as _noop } from 'lodash-es';
-// import { MatPaginator } from '@angular/material/paginator';
-// import { ToastrService } from 'ngx-toastr';
 
 export interface PeriodicElement {
   name: string;
@@ -27,6 +25,7 @@ export class DashboardComponent implements OnInit {
   full: boolean = true;
   limit = 10;
   Infinitescroll :any
+
   // Sorting
   @ViewChild(MatSort, { static: true }) sort: MatSort | undefined
   
@@ -51,7 +50,6 @@ export class DashboardComponent implements OnInit {
   }
 
   // Checkbox
-  /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
     const numSelected = this.selection.selected.length;
     const numRows = this.dataSource.data.length;
@@ -60,7 +58,6 @@ export class DashboardComponent implements OnInit {
     
   }
 
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
   masterToggle() {
     this.isAllSelected() ?
         this.selection.clear() :
@@ -68,26 +65,12 @@ export class DashboardComponent implements OnInit {
   }
 
   // Infinite Scroll
-  
-  // handleScroll = (scrolled: boolean) => {
-  //   console.timeEnd('lastScrolled');
-  //   scrolled ? this.dashboardService.getdashboard() : _noop();
-  //   console.time('lastScrolled');
-  // }
-  // hasMore = () => !this.dataSource || this.dataSource.data.length < this.limit;
-
-  // signout
-  // signOut(){
-  //   this.toast.success("Logout Succesfully")
-  // }
-
-  getdashboard:any;
+    
 onTableScroll(event:any) {
-  const tableViewHeight = event.target.offsetHeight // viewport: ~500px
-  const tableScrollHeight = event.target.scrollHeight // length of all table
-  const scrollLocation = event.target.scrollTop; // how far user scrolled
-  
-  // If the user has scrolled within 200px of the bottom, add more data
+  const tableViewHeight = event.target.offsetHeight 
+  const tableScrollHeight = event.target.scrollHeight 
+  const scrollLocation = event.target.scrollTop; 
+    
   const buffer = 200;
   const limit = tableScrollHeight - tableViewHeight - buffer;    
   if (scrollLocation > limit) {
@@ -97,18 +80,7 @@ onTableScroll(event:any) {
 }
 
 
-  // console.log("called")
-  // let params: any = {};
   
-  //   this.page = this.dashboardService.getdashboard() + this.limit;
-  //   params['offset'] = this.page;
-  //   params['limit'] = this.limit;
-  //     this.page_scroll = params['limit'];
-  //     
-  //     this.tabledata.sort = this.page_scroll;
-    
-  
-
 }
 
 
